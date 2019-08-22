d0cef292a818ffcf78ae17017e4ffa00ecbca4f7
said shah <saidshahdev@gmail.com>
2019-08-22 19:47:29 -0400

    finished address class

--- studentBlackboardSimple/Address.cs
+++ studentBlackboardSimple/Address.cs
@@ -2,6 +2,22 @@ namespace studentBlackboardSimple
 {
     public class Address
     {
-        // has a streetNumber, city, state, zip and streetName
+        // has a houseNumber, streetName, city, state, zip
+        
+        private int HouseNumber { get; set; }
+        private string City { get; set; }
+        private string State { get; set; }
+        private int Zipcode { get; set; }
+        private string StreetName { get; set; }
+        
+        public Address(int houseNumber, string streetName, string city, string state, int zipcode)
+        {
+            HouseNumber = houseNumber;
+            StreetName = streetName;
+            City = city;
+            State = state;
+            Zipcode = zipcode;
+        }
+        
     }
 } 
