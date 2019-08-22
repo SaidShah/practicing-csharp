1bad24e08e767699800259b656a069ee2e168dce
said shah <saidshahdev@gmail.com>
2019-08-21 21:17:57 -0400

    added fields and methods for name class

--- studentBlackboardSimple/Name.cs
+++ studentBlackboardSimple/Name.cs
@@ -1,7 +1,43 @@
+using System;
+
 namespace studentBlackboardSimple
 {
     public class Name
     {
             // has a firstName, lastName and middleInitial
+
+            private string FirstName { get; set; }
+            private string MiddleInitial { get; set; }
+            private string LastName { get; set; }
+
+            public Name(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            
+            public Name(string firstName, string middleInitial, string lastName)
+            {
+                FirstName = firstName;
+                MiddleInitial = middleInitial;
+                LastName = lastName;
+            }
+
+            private string GetMiddleInitial()
+            {
+                if (string.IsNullOrEmpty(MiddleInitial) || string.IsNullOrWhiteSpace(MiddleInitial))
+                {
+                    return "";
+                }
+                else
+                {
+                    return MiddleInitial;
+                }
+            }
+
+            public override string ToString()
+            {
+                return FirstName + " " + GetMiddleInitial() + " " + LastName;
+            }
     }
 }
--- studentBlackboardSimple/Program.cs
+++ studentBlackboardSimple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace studentBlackboardSimple
 {
@@ -6,6 +7,7 @@ namespace studentBlackboardSimple
     {
         static void Main(string[] args)
         {
+            
             
         }
     }
