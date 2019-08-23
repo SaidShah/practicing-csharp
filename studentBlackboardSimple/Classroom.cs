0b8488bbce76adfa049e89c5cf872993250dbf13
Said Shah <saidshahdev@gmail.com>
2019-08-23 08:16:58 -0400

    finished classroom class

--- studentBlackboardSimple/Classroom.cs
+++ studentBlackboardSimple/Classroom.cs
@@ -3,5 +3,26 @@ namespace studentBlackboardSimple
     public class Classroom
     {
         // has roomNumber, hasProjector, building, computerLab, and nunberOfSeats variables
+
+        private string RoomNumber { get; set; }
+        private bool HasProjector { get; set; }
+        private string BuildingNumber { get; set; }
+        private bool IsComputerLab { get; set; }
+        private int NumberOfSeats { get; set; }
+
+        public Classroom(string roomNumber, bool hasProjector, string buildingNumber, bool isComputerLab, int numberOfSeats)
+        {
+            RoomNumber = roomNumber;
+            HasProjector = hasProjector;
+            BuildingNumber = buildingNumber;
+            IsComputerLab = isComputerLab;
+            NumberOfSeats = numberOfSeats;
+        }
+        public Classroom(string roomNumber, bool hasProjector, string buildingNumber)
+        {
+            RoomNumber = roomNumber;
+            HasProjector = hasProjector;
+            BuildingNumber = buildingNumber;
+        }
     }
 }
