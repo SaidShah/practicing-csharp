200430121c2e1fa42d2dcef1b3facf14101951d8
said shah <saidshahdev@gmail.com>
2019-08-22 20:27:59 -0400

    finished course and textbook classes

--- studentBlackboardSimple/Course.cs
+++ studentBlackboardSimple/Course.cs
@@ -1,8 +1,25 @@
+using System.ComponentModel.Design;
+using System.Net.Mime;
+
 namespace studentBlackboardSimple
 {
     public class Course
     {
         // has title, courseNumber, numOfCredits variables
         // also has get and set for textbook isbn, and facultyID
+        
+        private string Title { get; set; }
+        private string CourseNumber { get; set; }
+        private Textbook Textbook { get; set; }
+        private int NumCredits { get; set; }
+        private string FaultyId { get; set; }
+
+        public Course(string title, string courseNumber, int numCredits)
+        {
+            Title = title;
+            CourseNumber = courseNumber;
+            NumCredits = numCredits;
+        }
+        
     }
 }
--- studentBlackboardSimple/Textbook.cs
+++ studentBlackboardSimple/Textbook.cs
@@ -3,5 +3,25 @@ namespace studentBlackboardSimple
     public class Textbook
     {
         // has title, Name, published, isbn, price
+
+        private string Title { get; set; }
+        private Name Name { get; set; }
+        private bool Published { get; set; }
+        public string Isbn { get; set; }
+        private string Price { get; set; }
+
+        public Textbook(string title, Name name, bool published, string isbn, string price)
+        {
+            Title = title;
+            Name = name;
+            Published = published;
+            Isbn = isbn;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title}, By: {Name}, ISBN: {Isbn} ${Price} ";
+        }
     }
 }
