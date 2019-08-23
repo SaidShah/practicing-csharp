fdb58d1d5219d8ba0e161fac1e47b6342112a6b0
Said Shah <saidshahdev@gmail.com>
2019-08-23 15:55:43 -0400

    finished grade class

--- studentBlackboardSimple/Grade.cs
+++ studentBlackboardSimple/Grade.cs
@@ -2,12 +2,20 @@ namespace studentBlackboardSimple
 {
     public class Grade
     {
-        // has a courseNumber, studentGPA and letterGrade
+        // has a courseNumber and letterGrade
         
         private string CourseNumber { get; set; }
-        private double GPA { get; set; }
         private string LetterGrade { get; set; }
-        
-        
+
+        public Grade(string courseNumber, string letterGrade)
+        {
+            CourseNumber = courseNumber;
+            LetterGrade = letterGrade;
+        }
+
+        public override string ToString()
+        {
+            return $"Course Number: {CourseNumber} Letter Grade: {LetterGrade}";
+        }
     }
 }
